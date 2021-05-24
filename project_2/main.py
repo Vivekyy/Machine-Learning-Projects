6217215f831b6cef02965d04740137bf82b582e4
Vivek Yanamadula <vivekyanamadula@gmail.com>
2021-05-23 22:23:45 -0700

    Got results for moon and roll datasets

--- project_2/main.py
+++ project_2/main.py
@@ -1,2 +1,30 @@
 from train import train
+import numpy as np
+from tqdm import tqdm
 
+def getPlots(kernel):
+
+    #Get pairs of C and gamma
+    Cs = np.array([.5,1,5])
+    Gs = np.array([.5,1,5])
+
+    C,G = np.meshgrid(Cs,Gs)
+    CG = np.vstack((C.flatten(),G.flatten())).T
+
+    for cg in tqdm(CG, desc='Kernel '+str(kernel), leave=False):
+        c=cg[0]
+        g=cg[1]
+
+        path = 'moons/'+str(kernel)+'/MK'+str(kernel)+'C'+str(c)+'G'+str(g)
+        train('moons_dataset.csv', C=c, gamma=g, kernel=kernel, plotpath=path)
+
+
+        path = 'rolls/'+str(kernel)+'/RK'+str(kernel)+'C'+str(c)+'G'+str(g)
+        train('rolls_dataset.csv', C=c, gamma=g, kernel=kernel, plotpath=path)
+
+if __name__=="__main__":
+    #getPlots('linear')
+    #getPlots(2)
+    #getPlots(3)
+    getPlots(5)
+    #getPlots('rbf')
--- project_2/train.py
+++ project_2/train.py
@@ -6,19 +6,21 @@ import matplotlib
 
 matplotlib.use('Agg')
 
-def train(dataset, C=1.0, gamma='scale', kernel='rbf', plotname='Temp'):
+def train(dataset, C=1.0, gamma='scale', kernel='rbf', plotpath='Temp'):
 
     X1,X2,Y = getData(dataset)
 
     X = np.vstack((X1,X2)).T
 
-    Xtrain = X[:8000]
-    Ytrain = Y[:8000]
-    Xtest = X[8000:]
-    Ytest = Y[8000:]
+    Xtrain = X[:7000]
+    Ytrain = Y[:7000]
+    Xtest = X[7000:]
+    Ytest = Y[7000:]
 
     if (kernel=='rbf'):
         model = SVC(C=C, kernel=kernel, gamma=gamma)
+    elif (kernel==1 or kernel=='linear'):
+        model = SVC(C=C, kernel='linear', gamma=gamma)
     else:
         model = SVC(C=C, kernel='poly', degree=kernel, gamma=gamma)
     
@@ -30,14 +32,15 @@ def train(dataset, C=1.0, gamma='scale', kernel='rbf', plotname='Temp'):
     Xbound = np.mgrid[0:1:.01, 0:1:.01].reshape(2,-1).T
     Ybound = model.predict(Xbound)
 
+    title = "Kernel "+str(kernel)+": C = "+str(C)+", Gamma = "+str(gamma)
     plt.scatter(Xbound[:,0], Xbound[:,1], c=Ybound)
-    plt.title(plotname)
+    plt.title(title)
     plt.xlabel("Accuracy: "+str(accuracy))
-    
-    plt.savefig(plotname+'.png')
+
+    plt.savefig(plotpath+'.png')
     
     return accuracy
 
 if __name__=="__main__":
-    accuracy = train('rolls_dataset.csv')
+    accuracy = train('moons_dataset.csv')
     print("Test Accuracy: ", accuracy)
